490835ebf59f73f92b3b6c920332d02f5f8ae521
Anuj Godase <godaseanuj@gmail.com>
2016-12-09 16:03:54 +0530

    django app with all apis and  auth done

--- DjangoBackend/meetings/urls.py
+++ DjangoBackend/meetings/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
 	url(r'^signup', views.signup, name='signup'),
 	url(r'^signin', views.signin, name='signin'),
 	url(r'^signout', views.signout, name='signout'),
+	url(r'^apiaddmeet', views.apiaddmeet, name='apiaddmeet'),
+	url(r'^apiviewmeetings', views.apiviewmeetings, name='apiviewmeetings'),
 	url(r'^view', views.viewmeetingsfromcouchbase, name='view'),
 	url(r'^add', views.addmeetingtocouchbase, name='add'),
-	url(r'^checksignin', views.checksignin, name='checksignin')
-	
 ]
--- DjangoBackend/meetings/views.py
+++ DjangoBackend/meetings/views.py
@@ -6,38 +6,43 @@ from django.db import IntegrityError
 from django.core import serializers
 import json, requests
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
 	response = "Hello"
 	data = json.dumps(response)
 	return HttpResponse(data, content_type = "application/json")
 
+@csrf_exempt
 def signup(request):
-	username = request.GET.get("username")
-	password = request.GET.get("password")
-	if username is None or password is None:
-		response = "Username or password missing"
-		data = json.dumps(response)
-	else: 
-		mail = request.GET.get("mail")
-		firstname = request.GET.get("firstname")
-		lastname = request.GET.get("lastname")
-		try:
-			user = User.objects.create_user(username=username, password=password)
-		except IntegrityError:
-			status = "unable to add user, such user already exists"
-		else:
-			user.is_active = True
-			user.is_staff = False
-			if firstname is not None:
-				user.first_name = firstname
-			if lastname is not None:
-				user.last_name = lastname
-			if mail is not None:
-				user.email = mail
-			user.save()
-			status = "User with username "+username+" saved successfully"	
-		data = json.dumps(status)
+	if request.method == "POST":
+		username = request.POST.get("username")
+		password = request.POST.get("password")
+		if username is None or password is None:
+			response = "Username or password missing"
+			data = json.dumps(response)
+		else: 
+			mail = request.POST.get("mail")
+			firstname = request.POST.get("firstname")
+			lastname = request.POST.get("lastname")
+			try:
+				user = User.objects.create_user(username=username, password=password)
+			except IntegrityError:
+				status = "unable to add user, such user already exists"
+			else:
+				user.is_active = True
+				user.is_staff = False
+				if firstname is not None:
+					user.first_name = firstname
+				if lastname is not None:
+					user.last_name = lastname
+				if mail is not None:
+					user.email = mail
+				user.save()
+				status = "User with username "+username+" saved successfully"	
+			data = json.dumps(status)
+	else:
+		data = "Invalid request method"
 	return HttpResponse(data, content_type = "application/json")
 
 def signin(request):
@@ -68,14 +73,134 @@ def signout(request):
 		data = json.dumps(response)
 	return HttpResponse(data, content_type = "application/json")
 
-def checksignin(request):
-	if request.user.is_authenticated():
-		print type(request.user)
-		response = True
-		data = json.dumps(response)
+@csrf_exempt
+def apiaddmeet(request):
+	if request.method=="POST":
+		username = request.POST.get("username")
+		password = request.POST.get("password")
+		if username is None or password is None:
+			data = "username or password missing"
+		else:
+			user = authenticate(username=username,password=password)
+			if user is None:
+				data = "Invalid authentication"
+			else:
+				meetdate = request.POST.get("meetdate")
+				meettime = request.POST.get("meettime")
+				location = request.POST.get("location")
+				meetingfor = request.POST.get("meetingfor")
+				if meetdate is None or meettime is None or location is None or meetingfor is None:
+					response = "One or more fields missing"
+					data = json.dumps(response)
+				else:
+					#create request
+						#url
+						#header
+						#data
+					url = 'http://localhost:4984/meetinggw/' 
+					#data -d
+					# data = '{"type": "user", "username": "mychannel4"}'
+					payload = {
+						'username':username,
+						'meetdate':meetdate,
+						'meettime':meettime,
+						'location':location,
+						'meetingfor':meetingfor
+					}
+					headers = {'content-type': 'application/json'}
+					
+					#make request
+					r = requests.post(url, data=json.dumps(payload), headers=headers)
+					# r = requests.post(url, data=data, headers=headers)
+					if r.status_code == 200:
+						response = "Meeting successfully created"
+					else:
+						response = "Something went wrong"
+					#show response
+						
+					data = json.dumps(response)
 	else:
-		response = False
-		data = json.dumps(response)
+		data = "Invalid request method"
+	return HttpResponse(data, content_type = "application/json")
+
+@csrf_exempt
+def apiviewmeetings(request):
+	if request.method=="POST":
+		username = request.POST.get("username")
+		password = request.POST.get("password")
+		if username is None or password is None:
+			data = "username or password missing"
+		else:
+			user = authenticate(username=username,password=password)
+			if user is None:
+				data = "Invalid authentication"
+			else:
+				host = "http://localhost:4984/"
+				db = "meetinggw/"
+				filter = "_changes?filter=sync_gateway/bychannel&channels="
+				query = str(username)
+				url = host+db+filter+str(query)
+				r = requests.get(url)
+				#fetchdata
+				if r.status_code == 200:
+					raw_json = r.json()#json.loads(r.text)
+					documents = []
+					docsmetadata = raw_json["results"]
+					requesteddocs = []
+					for i in docsmetadata:
+						if i["id"] != "_user/GUEST":
+							tempid = {"id": i["id"]}
+							requesteddocs.append(tempid)
+
+					#this is inefficient method 
+					
+					for i in docsmetadata:
+						if i["id"] != "_user/GUEST":
+							fetchid = i["id"]
+							fetchhost = "http://localhost:4984/"
+							fetchdb = "meetinggw/"
+							fetchurl = fetchhost+fetchdb+str(fetchid)
+							fetchret =requests.get(fetchurl)
+							if fetchret.status_code == 200:
+								# tempdoc = {'doc': fetchret.json()}
+								# documents.append(json.loads(fetchret.text))
+								# documents.append(tempdoc)
+								documents.append(fetchret.json())
+					#trying efficient method here
+					# requestdata = {"docs": requesteddocs}
+					# fetchrequrl = host+db+'_bulk_get'
+					# headers = {'content-type': 'application/json'}
+					# fetchreq = requests.post(fetchrequrl, data=json.dumps(requestdata), headers=headers)
+					
+					# if fetchreq.status_code == 200:
+					# 	split_data = fetchreq.text.split("Content-Type: application/json")
+					# 	for i in split_data:
+					# 		split_at_hypen = i.split("--")
+					# 		tempdoc = {"doc": split_at_hypen[0].strip()}					
+					# 		documents.append(tempdoc)
+					# 	next_last_sequence = raw_json["last_seq"] # for sync purpose
+					# 	# data = json.dumps(documents) this not needed here as documents are already json encoded
+						
+					# 	documentsdict = {}
+					# 	documentsdict['documents'] = documents
+					# 	last_seqdict = {}
+					# 	last_seqdict['last_seq'] = next_last_sequence
+					# 	datadump = [documentsdict, last_seqdict] 
+					# 	data = datadump
+					
+					docsdump = json.dumps(documents)
+					next_last_sequence = raw_json["last_seq"] # for sync purpose
+		 			
+					data = json.dumps({
+						"last_seq": next_last_sequence,
+						"documents": documents
+					})
+
+				else:
+					data = json.dumps("Error fetching data")
+
+	else:
+		data = "Invalid request method"
 	return HttpResponse(data, content_type = "application/json")
 
 def viewmeetingsfromcouchbase(request):
@@ -108,7 +233,6 @@ def viewmeetingsfromcouchbase(request):
 					fetchurl = fetchhost+fetchdb+str(fetchid)
 					fetchret =requests.get(fetchurl)
 					if fetchret.status_code == 200:
-						print fetchret.json()
 						# tempdoc = {'doc': fetchret.json()}
 						# documents.append(json.loads(fetchret.text))
 						# documents.append(tempdoc)
